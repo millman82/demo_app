41320cebb562e221d2b63c7bcd52c81abffc9533
Timothy Miller <timothy.miller@icloud.com>
2013-09-09 22:13:01 -0400

    Randomize Secret Token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,20 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = 'f3465f772c31a06ad7203d7edd0cf8e6be9df7716f9b8114c3b2473c64af18076decae9d7499feb63bef591a3894f3df6fcac070a6e0ac22df58b5df5e0708c9'
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
